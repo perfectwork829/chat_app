aa55d98c31fb58b0a672b3c0a7743760781c70b8
perfectwork829 <superdev829@gmail.com>
2023-07-01 18:39:13 +0900

    test

--- client/config.js
+++ client/config.js
@@ -2,6 +2,6 @@ const isDev = process.env.NODE_ENV === 'development';
 
 export default {
   isDev,
-  brandName: 'IMax',
+  brandName: 'Test APP',
   avatarUploadLimit: 2 * 1024 * 1024, // 2 MB
 };
--- server/db/connect.js
+++ server/db/connect.js
@@ -2,7 +2,7 @@ const { connect } = require('mongoose');
 const { isDev, db } = require('../config');
 
 module.exports = async () => {
-  try {   
+  try {
     const uri = isDev ? db.uri : db.uri;
     // const uri = isDev ? `mongodb://localhost:27017/${db.name}` : db.uri;
     await connect(uri);
